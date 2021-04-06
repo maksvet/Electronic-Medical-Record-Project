60f0abb3fea7566620e1ec9c1b6bcab9ddb5e32f
Donald Kurangwa <donkur@my.yorku.ca>
2021-04-06 14:17:51 -0400

    react with starter components, updated schema

---
+++ emr-app/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+    return (
+        <div>
+            <Button />
+        </div>
+    )
+}
+
+export default Header
+
